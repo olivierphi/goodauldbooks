c1b60042300c6d189c71e95603348436a2d3ad66
Olivier Philippon <olivier@rougemine.com>
2018-04-18 11:21:19 +0100

    "@types/react" now has `React.createContext()`, yeah!

--- client/src/contexts/lang.ts
+++ client/src/contexts/lang.ts
@@ -1,12 +1,3 @@
 import * as React from "react";
 
-// React.createcontext is not ready yet in @types/react... :-/
-
-const reactRef: any = React;
-
-export interface Context {
-  Provider: any;
-  Consumer: any;
-}
-
-export const CurrentLangContext: Context = reactRef.createContext();
+export const CurrentLangContext = React.createContext();
