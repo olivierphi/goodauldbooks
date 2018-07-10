dfde2d9e84dd2e8032bff5c9448eeae4956063b8
Olivier Philippon <olivier@rougemine.com>
2018-07-10 11:52:09 +0100

    add PG books rsync-and-store-in-DB process via a Python script

---
+++ server/pg-rdfs-indexing/bin/import-books-data.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import re
+import time
+import typing as t
+from pathlib import Path
+
+import click
+
+from pg_import import db as pg_db, parsing as pg_parsing
+
+RDF_FILE_REGEX = re.compile('^pg(\d+)\.rdf$')
+DB_BATCH_SIZE_DEFAULT = 150  # we will store books in DB only every N books
+
+
+@click.command()
+@click.argument('path', type=click.Path(exists=True, dir_okay=True, file_okay=False, readable=True))
+@click.option('--batch-size', default=DB_BATCH_SIZE_DEFAULT, type=click.INT)
+def find_and_process_pg_books(path: str, batch_size: int):
+    start_time = time.time()
+    nb_rdf_files_found = 0
+    nb_books_processed = 0
+    books_to_store_cur_batch: t.List[t.Dict] = []
+    handled_pg_books_ids: t.List[str] = []
+    for rdf_file in Path(path).glob('*/*.rdf'):
+
+        nb_rdf_files_found += 1
+        rdf_file_match = RDF_FILE_REGEX.fullmatch(rdf_file.name)
+        if not rdf_file_match:
+            continue
+
+        pg_book_id = rdf_file_match[1]
+
+        if pg_book_id in handled_pg_books_ids:
+            click.echo(f'PG book "{pg_book_id}" already handled (file "{rdf_file}"). Skipping...')
+            continue
+
+        book_processing_result = pg_parsing.process_book_from_pg_rdf_file(pg_book_id, rdf_file)
+
+        if not book_processing_result:
+            continue
+
+        pg_db.add_book_storage_params_to_current_batch(books_to_store_cur_batch, path, book_processing_result)
+
+        handled_pg_books_ids.append(pg_book_id)
+
+        nb_books_processed += 1
+
+        if nb_books_processed % batch_size == 0:
+            pg_db.execute_books_storage_in_db_batch(books_to_store_cur_batch)
+            books_to_store_cur_batch = []
+
+        if nb_books_processed % 100 == 0:
+            click.echo(str(nb_books_processed))
+
+    pg_db.execute_books_storage_in_db_batch(books_to_store_cur_batch)
+
+    click.echo(f'{nb_rdf_files_found} RDF files found, {nb_books_processed} saved into database.')
+    end_time = time.time()
+    click.echo(f'Took {end_time - start_time}s.')
+
+
+if __name__ == '__main__':
+    find_and_process_pg_books()
---
+++ server/pg-rdfs-indexing/src/pg_import/parsing.py
@@ -0,0 +1,77 @@
+import json
+import re
+import typing as t
+from enum import Enum
+from pathlib import Path
+
+
+class BookAssetType(Enum):
+    EPUB = 'epub'
+    MOBI = 'mobi'
+    TXT = 'txt'
+    COVER = 'cover'
+
+
+DEFAULT_BOOK_INTRO_LENGTH = 5000
+
+BOOK_ASSETS_FILES_TYPES: t.Dict[BookAssetType, t.Pattern] = {
+    BookAssetType.EPUB: re.compile('^pg\d+-images\.epub$'),
+    BookAssetType.COVER: re.compile('^pg\d+\.cover\.medium\.jpg$'),
+    BookAssetType.MOBI: re.compile('^pg\d+-images\.mobi$'),
+    BookAssetType.TXT: re.compile('^pg\d+\.txt\.utf8$'),
+}
+
+
+class BookAsset(t.NamedTuple):
+    size: int
+    type: BookAssetType
+    path: Path
+
+
+class BookProcessingResult(t.NamedTuple):
+    book_id: str
+    rdf_file_content: str
+    assets: t.Dict[BookAssetType, BookAsset]
+    intro: t.Optional[str]
+
+    def assets_as_json(self, books_root_path: str) -> str:
+        assets_as_simple_structures = {
+            type.value: {'path': str(data.path.relative_to(books_root_path)), 'size': data.size}
+            for (type, data) in self.assets.items()
+        }
+        return json.dumps(assets_as_simple_structures)
+
+
+def process_book_from_pg_rdf_file(pg_book_id: str, rdf_file: Path) -> t.Optional[BookProcessingResult]:
+    rdf_file_dir = rdf_file.parent
+    assets = get_pg_book_assets(rdf_file_dir)
+
+    if BookAssetType.EPUB not in assets:
+        return None
+
+    rdf_file_content = rdf_file.read_text(encoding='utf-8')
+
+    book_intro: t.Optional[str] = None if BookAssetType.TXT not in assets else get_book_intro(
+        assets[BookAssetType.TXT].path)
+
+    result = BookProcessingResult(
+        book_id=pg_book_id, rdf_file_content=rdf_file_content,
+        assets=assets, intro=book_intro
+    )
+
+    return result
+
+
+def get_book_intro(book_as_txt: Path, intro_length: int = DEFAULT_BOOK_INTRO_LENGTH) -> str:
+    with open(book_as_txt, 'r', encoding='utf-8') as f:
+        return f.read(intro_length)
+
+
+def get_pg_book_assets(rdf_file_dir: Path) -> t.Dict[BookAssetType, BookAsset]:
+    assets = {}
+    for sibling in rdf_file_dir.iterdir():
+        for asset_type, pattern in BOOK_ASSETS_FILES_TYPES.items():
+            if pattern.fullmatch(sibling.name):
+                book_asset = BookAsset(type=asset_type, size=sibling.stat().st_size, path=sibling.resolve())
+                assets[asset_type] = book_asset
+    return assets
