b3a06103e96cbb5635b4c43ad3022d3c3f468a4e
Olivier Philippon <olivier@rougemine.com>
2018-08-15 19:37:59 +0100

    [doc] add a ref to the "bin/netlify_build_client.sh" script in our "app-config.ts" file

--- client/src/app-config.ts
+++ client/src/app-config.ts
@@ -3,6 +3,8 @@
  * Yup, but we can adapt any of these parameters for staging/prod environments,
  * just by running a few "sed -i" on the bundled-by-Webpack JS files :-)
  * (or before running WebPack during the deployment process, and running "sed -i" only on this file. Whatever...)
+ *
+ * @see the "bin/netlify_build_client.sh" script, where we actually do those "sed -i" during the deployment to Netlify
  */
 
 export const AppConfig = {
