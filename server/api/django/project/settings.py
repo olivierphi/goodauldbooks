0181a660d9ecdc342139978748f6e3c23e3db186
Olivier Philippon <olivier@rougemine.com>
2018-08-15 18:03:27 +0100

    [Django] allow all hosts in terms of CORS policy (we may be more subtle later on)

--- server/api/django/project/settings.py
+++ server/api/django/project/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,6 +142,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS (Cross-Origin Resource Sharing) management
+# https://github.com/ottoyiu/django-cors-headers
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 # pylint: disable=E0401,E0402,E0602
 
