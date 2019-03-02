dc44875ba645c8d4594cf3fcd596497c98c4a922
Olivier Philippon <olivier@rougemine.com>
2019-03-02 20:18:18 +0000

    [admin] minor fine-tuning

--- src/app/library/admin.py
+++ src/app/library/admin.py
@@ -1,5 +1,8 @@
+import typing as t
+
 from django.contrib import admin
 from django import forms
+from django.utils.formats import number_format
 
 from . import models
 
@@ -21,10 +24,21 @@ class BookAdminForm(forms.ModelForm):
 @admin.register(models.Book)
 class BookAdmin(admin.ModelAdmin):
     form = BookAdminForm
-    list_display = ("public_id", "lang", "title", "subtitle", "highlight")
+    list_display = ("public_id", "lang", "title", "subtitle", "highlight", "weight")
+    list_display_links = ("public_id", "title")
     search_fields = ("public_id", "title")
     autocomplete_fields = ("authors", "genres")
-    readonly_fields = ("public_id", "slug")
+    readonly_fields = ("public_id", "slug", "size")
+    list_filter = ("highlight",)
+    list_per_page = 30
+
+    def weight(self, obj: models.Book) -> t.Optional[str]:
+        if obj.size is None:
+            return None
+        return (
+            number_format(round(obj.size / 1024), use_l10n=True, force_grouping=True)
+            + " kb"
+        )
 
 
 @admin.register(models.Author)
@@ -32,9 +46,11 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ("public_id", "first_name", "last_name")
     search_fields = ("public_id", "first_name", "last_name")
     readonly_fields = ("public_id", "slug")
+    list_per_page = 30
 
 
 @admin.register(models.Genre)
 class GenreAdmin(admin.ModelAdmin):
     search_fields = ("name",)
     readonly_fields = ("id", "slug")
+    list_per_page = 30
