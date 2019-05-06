2681be794b0cfb549dae1463ea77d5a367b7dd78
Olivier Philippon <olivier@rougemine.com>
2019-05-06 20:43:31 +0100

    [website][frontend] replace the JavaScript ugly hack of books covers with a good ol' <a>

--- src/app/website/css_js_src/src/main.js
+++ src/app/website/css_js_src/src/main.js
@@ -1,12 +1,10 @@
 import "./scss/main.scss";
 import Turbolinks from "turbolinks";
-import { initBooksListBehaviour } from "./behaviour/books-list";
 
 window.addEventListener("load", initApp);
 
 function initApp() {
   initAjaxNav();
-  initBooksListBehaviour();
 }
 
 function initAjaxNav() {
