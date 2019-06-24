d919736a2cfebe0d520e463560cca5369f5f0932
Olivier Philippon <olivier@rougemine.com>
2019-06-24 19:13:58 +0100

    Finally, I found the proper way to query Models relationships without having to manually "join" ^_^

--- app/Library/BookRepository.php
+++ app/Library/BookRepository.php
@@ -10,15 +10,17 @@ class BookRepository
 {
     public function booksByAuthor(int $authorId): Builder
     {
-        return Book::join('authors_books', 'authors_books.book_id', '=', 'books.id')
-            ->where('authors_books.author_id', '=', $authorId)
+        return Book::whereHas('authors', function (Builder $authorQuery) use ($authorId) {
+            $authorQuery->where('id', $authorId);
+        })
             ->orderBy('books.title');
     }
 
     public function booksByGenre(int $genreId): Builder
     {
-        return Book::join('books_genres', 'books_genres.book_id', '=', 'books.id')
-            ->where('books_genres.genre_id', '=', $genreId)
+        return Book::whereHas('genres', function (Builder $genreQuery) use ($genreId) {
+            $genreQuery->where('id', $genreId);
+        })
             ->orderBy('books.title');
     }
 }
